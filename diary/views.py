e4e40fd4dd0144fdd18aa589a1d3c0271781a9bb
KoukiNakano <ksw2070305@stu.o-hara.ac.jp>
2021-09-13 12:26:30 +0900

    日記作成機能

--- diary/views.py
+++ diary/views.py
@@ -42,3 +42,23 @@ class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     model = Diary
     template_name = 'diary_detail.html'
     #pk_url_kwarg = 'id'
+
+
+from.forms import InquiryForm, DiaryCreateForm
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました。")
+        return super().form_invalid(form)
